2f3f6b1c68a7c0f08d2798c2c220d04e0fe0d042
world666 <world666.kirillov@mail.ru>
2014-05-03 13:56:42 +0300

    kyrylov added versions service

---
+++ StrategyServices/Versions/IVersionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyServices.Versions
+{
+    [ServiceContract]
+    public interface IVersionService
+    {
+        [OperationContract]
+        string GetCommandString(int i);
+    }
+}
